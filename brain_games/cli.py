52a33cfd30428cd66e39fa47a8abe1c2f5ca8bfc
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-07-12 19:16:29 +0300

    Fix docstrings

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,10 @@
+# cli.py
+"""Module for green user in start program"""
+
 import prompt
 
+
 def welcome_user():
+    """Ask user name and greet"""
     name = prompt.string('May I have your name? ')
     print(f'Nice to meet you {name}')
