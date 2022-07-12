b2b9c0c3bee30c77a24fe6fa497681b2fbd1ab50
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-07-12 19:19:11 +0300

    Remove greet() from general code

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-from brain_games import cli
+"""Module for start The Game"""
+
+# from brain_games import cli
 
 
 def main():
+    """User greet function"""
     print("Welcome to the Brain Games!")
-    cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
 
-#END
-
+# END
