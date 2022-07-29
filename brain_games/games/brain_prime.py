dc6fba3d3ff42fd75148dcf1ec8073588be22b4b
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-07-29 15:27:01 +0300

    Fix bug in a prime number game

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,14 +6,13 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime_number():
-    list_of_primes = [2, 3, 5, 7]
-    rm = random.randint(1, 50)
-    print("Question: {}".format(rm))
-    if rm in list_of_primes:
-        return True
-    else:
-        cor = rm % 2 == 0 or rm % 5 == 0 or rm % 7 == 0 or rm % 3 == 0
-        correct_answer = not(cor)
+    num = random.randint(1, 50)
+    print("Question: {}".format(num))
+    for i in range(2, num):
+        if (num % i) == 0:
+            correct_answer = False
+        else:
+            correct_answer = True
         return 'yes' if correct_answer else 'no'
 
 
