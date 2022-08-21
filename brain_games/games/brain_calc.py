ed43ad9a3da4bd34ddb3906fff5f5faa20a7e5ae
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-22 01:18:00 +0300

    Fix #5 from code-review
    Replace eval to operators add, sub and mul in get_question_and_answer function

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,15 +1,20 @@
+# Import build-in modules
 import random
+import operator
+
 
 DESCRIPTION = 'What is the result of the expression?'
 
 
 def get_question_and_answer():
     """Create randon +, _ or * with 2 numbers and check answer"""
-    actions = ['+', '-', '*']
+    actions = {'+': operator.add, '*': operator.mul, '-': operator.sub}
     first_num = random.randint(1, 14)
     second_num = random.randint(1, 14)
-    random_action = random.choice(actions)
+    random_action = random.choice(list(actions))
     math_question = f'{first_num} {random_action} {second_num}'
+    correct_answer = actions[random_action](first_num, second_num)
+
     print('Question: {}'.format(math_question))
-    correct_answer = eval(math_question)
+
     return correct_answer
