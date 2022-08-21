dbfbf11f2b04b07c9d7cde88f4732399c74826e5
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-22 02:17:57 +0300

    Fix #8 from code-review

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -17,11 +17,17 @@ def get_question_and_answer():
     highest_number = list_of_numbers[-1]
     lowest_number = list_of_numbers[0]
 
-    highest_number_gsd = [num for num in range(1, highest_number + 1)
-                          if highest_number % num == 0]
-    lowest_number_gsd = [num for num in highest_number_gsd
-                         if lowest_number % num == 0]
-
-    correct_answer = max(lowest_number_gsd)
+    correct_answer = find_gcd(lowest_number, highest_number)
 
     return correct_answer, math_question
+
+
+def find_gcd(low_num, high_num):
+    """Function find GCD for two numbers and return GSD"""
+    highest_number_gcd = [num for num in range(1, high_num + 1)
+                          if high_num % num == 0]
+    lowest_number_gcd = [num for num in highest_number_gcd
+                         if low_num % num == 0]
+    gcd = max(lowest_number_gcd)
+
+    return gcd
