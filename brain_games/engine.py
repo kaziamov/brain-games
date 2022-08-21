c64f02fdd380e3ec5a354c529aecdd29daa547d5
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-22 02:04:53 +0300

    Fix #3 from code-review
    Change game logic. Remove game_is_on variable. Use return for lose game.

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# Import third-party modules
 import prompt
 
 NUMBER_OF_QUESTIONS = 3
@@ -13,31 +14,33 @@ def run_game(game):
     name = prompt.string()
     print('Hello, {}'.format(name))
 
-    game_is_on = True
+    # game_is_on = True
     print(game.DESCRIPTION)
 
+    number_of_correct_answers = 0
+
     for i in range(NUMBER_OF_QUESTIONS):
-        if game_is_on:
-            correct_answer, question_message = game.get_question_and_answer()
+        # if game_is_on:
+        correct_answer, question_message = game.get_question_and_answer()
 
-            print('Question: {}'.format(question_message))
+        print('Question: {}'.format(question_message))
 
-            if correct_answer == 'yes' or correct_answer == 'no':
-                user_answer = prompt.string('Your answer: ')
-            else:
-                user_answer = prompt.integer('Your answer: ')
+        if correct_answer == 'yes' or correct_answer == 'no':
+            user_answer = prompt.string('Your answer: ')
+        else:
+            user_answer = prompt.integer('Your answer: ')
 
-            message = "'{}' is wrong answer ;(. Correct answer was '{}'"
-            if correct_answer == user_answer:
-                print('Correct!')
-                user_answer = True
-            else:
-                print(message.format(user_answer, correct_answer))
-                user_answer = False
+        message = "'{}' is wrong answer ;(. Correct answer was '{}'"
+        if correct_answer == user_answer:
+            print('Correct!')
+            # user_answer = True
+            number_of_correct_answers += 1
+        else:
+            print(message.format(user_answer, correct_answer))
+            # user_answer = False
+            print("""Let's try again, {}!""".format(name))
+            return
 
-            game_is_on = user_answer
-
-    if game_is_on:
-        print('Congratulations, {}!'.format(name))
-    else:
-        print("""Let's try again, {}!""".format(name))
+    # if number_of_correct_answers == NUMBER_OF_QUESTIONS:
+    print('Congratulations, {}!'.format(name))
+    # return
