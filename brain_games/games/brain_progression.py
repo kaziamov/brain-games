66809f39cdaae2bea300de66822c7b32f50bb948
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-22 03:12:37 +0300

    Fix linter errors

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,14 +5,15 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 def get_question_and_answer():
     """Generate missing in the progression"""
-    start = random.randint(3, 9)
+    start_number = random.randint(3, 9)
     step = random.randint(3, 9)
     length = 7
 
-    numbers = [num for num in range(start, (start + length * step), step)]
+    numbers = [num for num in range(start_number, (start_number + length * step), step)]
     correct_answer = (random.choice(numbers))
 
-    progression = ' '.join([str(n) if n != correct_answer else '..' for n in numbers])
+    progression = ' '.join([str(number) if number != correct_answer
+                            else '..' for number in numbers])
     math_message = f'{progression}'
 
     return correct_answer, math_message
