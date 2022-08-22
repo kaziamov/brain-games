1930d478999bfc179b67079f9a5aae00e68a43bd
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-22 03:33:42 +0300

    Add docstring

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -4,6 +4,8 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
+    """Create random number and check is prime or not.
+    Return True or False"""
     num = random.randint(2, 50)
     correct_answer = is_prime(num)
     math_message = ("{}".format(num))
