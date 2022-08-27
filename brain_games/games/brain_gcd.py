dfdda9380f3e20f32fcd6bfe94e90da8c4c9cd9e
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-08-28 00:41:02 +0300

    Fix #6 from code-review #5
    Refactor find_gcd function

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -22,12 +22,8 @@ def get_question_and_answer():
     return correct_answer, math_question
 
 
-def find_gcd(low_num, high_num):
+def find_gcd(num1, num2):
     """Function find GCD for two numbers and return GSD"""
-    highest_number_gcd = [num for num in range(1, high_num + 1)
-                          if high_num % num == 0]
-    lowest_number_gcd = [num for num in highest_number_gcd
-                         if low_num % num == 0]
-    gcd = max(lowest_number_gcd)
-
-    return gcd
+    if not num2:
+        return num1
+    return find_gcd(num2, num1 % num2)
