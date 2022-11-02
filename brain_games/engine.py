58744e30c6c4dcf0e88d3d0a308018c79c950410
Ilia Kaziamov <ilya.kazyamov@gmail.com>
2022-11-02 21:34:29 +0300

    Fix typo

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -12,7 +12,7 @@ def run_game(game):
 
     print("Welcome to the Brain Games!")
 
-    name = prompt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
 
     print(f'Hello, {name}')
     print(game.DESCRIPTION)
@@ -27,7 +27,7 @@ def run_game(game):
         if correct_answer != user_answer:
             print(
                 f"'{user_answer}' is wrong answer ;(."
-                f"Correct answer was '{correct_answer}'"
+                f"Correct answer was '{correct_answer}'\n"
                 f"Let's try again, {name}!"
             )
             return
